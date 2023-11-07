a1cb50748396ff5fd75c4ec0ddab7ff7ff406ae1
Samir Pandey <samee.pan1994@gmail.com>
2023-11-07 12:04:24 -0800

    Initial Commit

---
+++ Memorizada/MemorizadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizadaApp.swift
+//  Memorizada
+//
+//  Created by Samir Pandey on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
